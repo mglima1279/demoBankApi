d3ed59e962a187bb830c7ba217d04f16d05fcc2e
Miguel Lima <mglima1279@gmail.com>
2026-05-28 15:47:02 -0300

    UserRepository done

---
+++ demoBankApi/Repositories/UserRepository.cs
@@ -0,0 +1,53 @@
+﻿using demoBankApi.Data;
+using demoBankApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace demoBankApi.Repositories
+{
+    public class UserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> Save(User entity)
+        {
+            if (entity.Id == 0)
+            {
+                _context.Users.Add(entity);
+            }
+            else
+            {
+                _context.Users.Update(entity);
+            }
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<User?> FindById(long Id)
+        {
+            return await _context.Users.FindAsync(Id);
+        }
+
+        public async Task<List<User>> FindAll()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<List<User>> FindByEmail(string Username)
+        {
+            return await _context.Users.Where(u=>u.Username == Username).ToListAsync();
+        }
+
+        public void DeleteById(long Id)
+        {
+            User Placeholder = new User();
+            Placeholder.Id = Id;
+
+            _context.Users.Remove(Placeholder);
+        }
+    }
+}
